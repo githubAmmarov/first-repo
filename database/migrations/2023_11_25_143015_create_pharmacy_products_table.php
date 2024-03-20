cb8737023e9f66bf0386dee5d509d950ce63b951
unknown <ammoooralkhja@gmail.com>
2024-03-20 15:43:50 +0300

    added a new colomn for pharmacy products table (quality)

--- database/migrations/2023_11_25_143015_create_pharmacy_products_table.php
+++ database/migrations/2023_11_25_143015_create_pharmacy_products_table.php
@@ -24,6 +24,7 @@ return new class extends Migration
                 ->cascadeOnUpdate();
 
             $table->integer('quantity');
+            $table->string('quality');
             $table->timestamps();
         });
     }
